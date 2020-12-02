7e0f33d2b68cf0d93fd89b892e57e00b8446712d
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-02 09:12:10 -0300

    created Entities/Enums

---
+++ ProgramaComercio/Entities/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProgramaComercio.Entities.Enums;
+
+namespace ProgramaComercio.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                double total = item.SubTotal();
+                sum += total;
+                
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ProgramaComercio/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgramaComercio.Entities
+{
+    class Product
+    {
+        public string NameProduct { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string nameProduct, double price)
+        {
+            NameProduct = nameProduct;
+            Price = price;
+        }
+    }
+}
